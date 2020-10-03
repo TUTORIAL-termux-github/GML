a93b18a28d4d12c3a9d45820ba1ed0ea42be222b
TUTORIAL-termux-github <71549156+TUTORIAL-termux-github@users.noreply.github.com>
2020-10-03 14:57:40 +0700

    Update GML.py

--- GML.py
+++ GML.py
@@ -1,2 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+# Author: R3D BULL BAR²
+# Youtube: R3D BULL CYBER
+# Jangan Mengedit tool ini tanpa seizin saya
 
+import os,sys,socket
+os.system('clear')
+os.system('pkg install python')
+os.system('pkg install figlet')
+os.system('pkg install nano')
+os.system('clear')
+print "[√] Installasi Selesai"
+os.system('sleep 2')
+print "[•] Selamat Datang di GML Tool"
+os.system('clear')
+os.system('toilet -f slant --gay "GML')
+print "============================="
+print "||=Coding By R3D BULL BAR²=||"
+print "||===R3D BULL CYBER TEAM===||"
+print "||===My friend:Mr.G3MBUL===||"
+print "||==========Mr.Mza=========||"
+print "============================="
